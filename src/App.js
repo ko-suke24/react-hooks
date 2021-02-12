bce4486bfbb9ea5025b4925598082d59fdbccb24
ko-suke24 <kosuke0322@gmail.com>
2021-02-12 15:02:54 +0900

    first react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count +1)
+  const decrement = () => setCount(count -1)
+
+  const reset = () => setCount(0)
+  const double= () => setCount(count *2)
+
   return (
-    <div>
-      This is template React app
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+        <div>
+          <button onClick={increment}>+1</button>
+          <button onClick={decrement}>-1</button>
+        </div>
+        <div>
+          <button onClick={reset}>Reset</button>
+          <button onClick={double}>×2</button>
+        </div>
+
+
+    
+
+    </React.Fragment>
   )
 }
 
